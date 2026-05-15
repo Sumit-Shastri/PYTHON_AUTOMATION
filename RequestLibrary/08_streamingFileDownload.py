4bdcacfd68026587823b5a895f217773d5cd57dc
Sumit-Shastri <shastrisumit25@gmail.com>
2026-05-15 12:51:19 +0530

    streaming of downloading files

---
+++ RequestLibrary/08_streamingFileDownload.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://www.win-rar.com/fileadmin/winrar-versions/winrar-x64-722ar.exe"
+
+r = requests.get(url)
+totalExpectedBytes = r.headers[('Content-Length')]
+print("total content length : ", totalExpectedBytes)
+
+bytesRecieved = 0
+
+with open("winrar.exe", 'wb') as f:
+    for chunk in r.iter_content(chunk_size = 128):
+        print(f"{bytesRecieved} recieved out of {totalExpectedBytes}")
+        f.write(chunk)
+        bytesRecieved = bytesRecieved + 128
+
