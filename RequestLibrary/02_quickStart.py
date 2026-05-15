74be80afb48a510083445183b16e615ba7ebc433
Sumit-Shastri <shastrisumit25@gmail.com>
2026-05-15 11:43:52 +0530

    quick start for requests

---
+++ RequestLibrary/02_quickStart.py
@@ -0,0 +1,12 @@
+import requests
+
+r = requests.get('https://api.github.com/events')
+print(r.status_code)
+
+print(r.headers['content-type'])
+
+print(r.encoding)
+
+print(r.text)
+
+print(r.json())
